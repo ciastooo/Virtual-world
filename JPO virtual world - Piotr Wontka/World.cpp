6a4d90d7453c816a8b5076cae40bfbe2a7dd3874
Ciastooo <leene4@gmail.com>
2018-01-02 11:41:00 +0100

    Loading world state from a file close #14

--- JPO virtual world - Piotr Wontka/World.cpp
+++ JPO virtual world - Piotr Wontka/World.cpp
@@ -4,6 +4,16 @@
 #include <windows.h>
 #include <fstream>
 #include <string>
+// Animals:
+#include "Wolf.h" // W
+#include "Sheep.h" // S
+#include "Fox.h" // F
+#include "Lion.h" // L
+#include "Rabbit.h" // R
+// Plants:
+#include "Grass.h" // g
+#include "Wolfberry.h" // w
+#include "Thorn.h" // t
 
 using namespace std;
 
@@ -34,7 +44,7 @@ World::World() : height(20), width(20)
 }
 World::~World()
 {
-	delete organisms;
+	delete this->organisms;
 }
 void World::draw() {
 	cout << "Piotr Wontka 167951" << endl;
@@ -95,5 +105,75 @@ void World::exportToFile() {
 	this->organisms->writeToFile(file);
 	file.close();
 	cout << endl << "Sukces! Plik zosta³ zapisany pod nazw¹ " << fileName << ".txt" << endl;
+	getchar();
+}
+void World::importFromFile() {
+	system("cls");
+	string fileName;
+	cout << "Podaj nazwê pliku do którego z którego ma zostaæ odczytany stan œwiata: ";
+	cin >> fileName;
+	ifstream file;
+	file.open(fileName + ".txt");
+	if (file.is_open()) {
+		delete this->organisms;
+		this->organisms = new OrganismList();
+		int x, y;
+		char organism;
+		Organism* importingOrganism = nullptr;
+		file >> x;
+		file >> y;
+		cout << "Œwiat o szerokoœci = " << x << "   oraz wysokoœci = " << y << endl;
+		this->width = x;
+		this->height = y;
+		while (!file.eof()) {
+			file >> organism;
+			file >> x;
+			file >> y;
+			switch(organism) {
+			case 'W':
+				cout << "Wilk: ";
+				importingOrganism = new Wolf(this, x, y);
+				break;
+			case 'S':
+				cout << "Owca: ";
+				importingOrganism = new Sheep(this, x, y);
+				break;
+			case 'F':
+				cout << "Lis: ";
+				importingOrganism = new Fox(this, x, y);
+				break;
+			case 'L':
+				cout << "Lew: ";
+				importingOrganism = new Lion(this, x, y);
+				break;
+			case 'R':
+				cout << "Królik: ";
+				importingOrganism = new Rabbit(this, x, y);
+				break;
+			case 'g':
+				cout << "Trawa: ";
+				importingOrganism = new Grass(this, x, y);
+				break;
+			case 'w':
+				cout << "Wilcze jagody: ";
+				importingOrganism = new Wolfberry(this, x, y);
+				break;
+			case 't':
+			default:
+				cout << "Cierñ: ";
+				importingOrganism = new Thorn(this, x, y);
+				break;
+			}
+			cout << "na pozycji x = " << x << "  y = " << y << endl;
+			if (importingOrganism != nullptr) {
+				this->organisms->insert(importingOrganism);
+			}
+		}
+		cout << endl << "Sukces! Plik zosta³ odczytany" << endl;
+	}
+	else {
+		cout << "Plik o podanej nazwie nie istnieje" << endl;
+	}
+	file.close();
 	getchar();
 }
--- JPO virtual world - Piotr Wontka/World.h
+++ JPO virtual world - Piotr Wontka/World.h
@@ -19,4 +19,5 @@ public:
 	void insertOrganism(Organism*);
 	Organism* getOrganismFromPosition(int x, int y);
 	void exportToFile();
+	void importFromFile();
 };
